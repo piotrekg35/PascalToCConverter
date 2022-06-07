4c3b147553769520d5161ef3cbe2d8e053e54a80
piotrekg35 <72696860+piotrekg35@users.noreply.github.com>
2022-06-07 11:34:52 +0200

    Update pascaltocconverter.py

--- pascaltocconverter.py
+++ pascaltocconverter.py
@@ -134,130 +134,118 @@ lexer.input(data)
 # for token in lexer:
 # print("line %d: %s(%s)" %(token.lineno, token.type, token.value))
 
-id_list = []
-type1 = ""
-
-
+id_list=[]
+type1=""
+text=[]
+pf_type=[]
+id_list_pom=[]
+isMain=[True]
+statement_seq=[]
+constdef=[]
 def p_pascal_program(p):
     'pascal_program : pogram_header SEMI_COLON block DOT'
     pass
-
-
 def p_program_header(p):
     'pogram_header : PROGRAM_SYM ID'
-    print("//Program: " + p[2])
+    print("//Program: "+p[2])
     print("#include <stdio.h>")
     print("#include <stdbool.h>")
-
-
 def p_block(p):
     'block : const_block type_block var_block procedure_and_function_block operation_block'
     pass
-
-
-constdef = []
-
 
 def p_const_block(p):
     '''const_block : CONST_SYM const_def SEMI_COLON const_def_list
                     | empty'''
 
-
 def p_const_def_list(p):
     '''const_def_list : const_def_list const_def SEMI_COLON
                       | empty'''
     pass
-
-
 def p_const_def(p):
     'const_def : id EQUAL const_value'
-    if str(constdef[-1])[0] == '\"':
-        print("const char " + str(constdef[0]) + "[" + str(len(str(constdef[-1]))) + "] = " + str(constdef[-1]) + ";")
+    if str(constdef[-1])[0]=='\"':
+        print("const char "+str(constdef[0])+"["+str(len(str(constdef[-1])))+"] = "+str(constdef[-1])+";")
     elif str(constdef[-1]).isnumeric():
-        print("const int " + str(constdef[0]) + " = " + str(constdef[-1]) + ";")
+        print("const int " + str(constdef[0]) + " = " + str(constdef[-1])+";")
     else:
-        print("const double " + str(constdef[0]) + " = " + str(constdef[-1]) + ";")
+        print("const double " + str(constdef[0]) + " = " + str(constdef[-1])+";")
     constdef.clear()
-
-
 def p_id(p):
     'id : ID'
     constdef.append(p[1])
-
 
 def p_id1(p):
     'id1 : ID'
     print(p[1], end=" ")
 
-
+def p_id2(p):
+    'id2 : ID'
+    statement_seq.append(str(p[1]) + " = ")
 def p_const_value(p):
     '''const_value :  real_number
                     | sign real_number
                     | TEXT'''
-    if p[1] is not None and str(p[1])[0] == '\'':
-        x = str(p[1])
-        constdef.append(x.replace('\'', '\"'))
-
-
+    if p[1]is not None and str(p[1])[0]=='\'':
+        x=str(p[1])
+        constdef.append(x.replace('\'','\"'))
 def p_sign(p):
     '''sign : OPER_ADD
                     | OPER_SUB'''
-    if len(constdef) == 1:
+    if len(constdef)==1:
         constdef.append(p[1])
-    elif len(constdef) > 1:
+    elif len(constdef)>1:
         constdef.append(str(constdef.pop(-1)) + str(p[1]))
-
-
+    if len(statement_seq)>0:
+        statement_seq[-1]=statement_seq[-1]+str(p[1])
 def p_real_number(p):
     '''real_number : num
                     | num dot num
                     | num e sign num
                     | num dot num e sign num'''
-
-
 def p_num(p):
     '''num : NUMBER'''
-    if len(constdef) == 1:
+    if len(constdef) == 1 or len(constdef) >0 and  str(str(constdef[-1])[-1]).isdigit():
         constdef.append(p[1])
     elif len(constdef) > 1:
         constdef.append(str(constdef.pop(-1)) + str(p[1]))
-
-
+    if len(statement_seq)>0:
+        statement_seq[-1]=statement_seq[-1]+str(p[1])
 def p_dot(p):
     '''dot : DOT'''
-    constdef.append(str(constdef.pop(-1)) + str(p[1]))
-
-
+    if len(constdef)>0:
+        constdef.append(str(constdef.pop(-1)) + str(p[1]))
+    if len(statement_seq)>0:
+        statement_seq[-1]=statement_seq[-1]+str(p[1])
 def p_e(p):
     'e : ID'
-    if p[1] != 'e':
+    if p[1] !='e':
         print("Syntax error in input!")
     else:
-        constdef.append(str(constdef.pop(-1)) + str(p[1]))
+        if len(constdef) > 0:
+            constdef.append(str(constdef.pop(-1)) + str(p[1]))
+        if len(statement_seq) > 0:
+            statement_seq[-1] = statement_seq[-1] + str(p[1])
+
+
 
 
 def p_type_block(p):
     '''type_block : TYPE_SYM type_def SEMI_COLON type_def_list
                     | empty'''
     pass
-
-
 def p_type_def_list(p):
     '''type_def_list : type_def_list type_def SEMI_COLON
                     | empty'''
     print(';')
-
-
 def p_type_def(p):
     '''type_def : id_list EQUAL type_denoter_td'''
     for i in id_list:
-        if i != id_list[-1]:
-            print(i, end=", ")
+        if i!= id_list[-1]:
+            print(i,end=", ")
         else:
-            print(i, end="")
+            print(i,end="")
     id_list.clear()
-
-
 def p_type_denoter(p):
     '''type_denoter : type_general
                     | enumerated_type
@@ -265,141 +253,106 @@ def p_type_denoter(p):
                     | record_type
                     | id1'''
     pass
-
-
 def p_type_denoter_td(p):
     '''type_denoter_td : type_general_td
                     | enumerated_type_td
                     | array_type_td
                     | record_type_td'''
     pass
-
-
 def p_type_general(p):
     '''type_general : CHAR_SYM
                     | INTEGER_SYM
                     | REAL_SYM
                     | BOOLEAN_SYM'''
     if p[1] == 'integer':
-        print("int", end=" ")
-        type1 = "int"
+        print("int",end=" ")
+        type1 ="int"
     elif p[1] == 'real':
         print("double", end=" ")
-        type1 = "double"
+        type1 ="double"
     elif p[1] == 'char':
         print("char", end=" ")
-        type1 = "char"
+        type1 ="char"
     elif p[1] == 'boolean':
         print("bool", end=" ")
         type1 = "bool"
-
-
 def p_type_general_td(p):
     '''type_general_td : CHAR_SYM
                     | INTEGER_SYM
                     | REAL_SYM
                     | BOOLEAN_SYM'''
     if p[1] == 'integer':
-        print("typedef int", end=" ")
-        type1 = "int"
+        print("typedef int",end=" ")
+        type1 ="int"
     elif p[1] == 'real':
         print("typedef double", end=" ")
-        type1 = "double"
+        type1 ="double"
     elif p[1] == 'char':
         print("typedef char", end=" ")
-        type1 = "char"
+        type1 ="char"
     elif p[1] == 'boolean':
         print("typedef bool", end=" ")
         type1 = "bool"
-
-
 def p_enumerated_type(p):
     '''enumerated_type : NAWL id_list NAWR'''
-    print("enum " + id_list.pop(0) + " {", end="")
+    print("enum "+id_list.pop(0)+" {",end="")
     for i in id_list:
-        if i != id_list[-1]:
-            print(i, end=", ")
+        if i!= id_list[-1]:
+            print(i,end=", ")
         else:
-            print(i, end="")
+            print(i,end="")
     print("}", end="")
     id_list.clear()
-
-
 def p_enumerated_type_td(p):
     '''enumerated_type_td : NAWL id_list NAWR'''
-    print("typedef enum " + " {", end="")
+    print("typedef enum "+" {",end="")
     for i in id_list:
-        if i == id_list[0]:
+        if i== id_list[0]:
             continue
-        if i != id_list[-1]:
-            print(i, end=", ")
+        if i!= id_list[-1]:
+            print(i,end=", ")
         else:
-            print(i, end="")
-    print("}" + id_list.pop(0), end="")
+            print(i,end="")
+    print("}"+id_list.pop(0), end="")
     id_list.clear()
-
-
 def p_id_list(p):
     '''id_list : ID
                 | id_list COMMA ID'''
     if p[1] is not None:
         id_list.append(p[1])
-    else:
-        id_list.append(p[3])
-
-
+    else: id_list.append(p[3])
 def p_array_type(p):
     '''array_type : ARRAY_SYM NAWKL NUMBER DOUBLE_DOT NUMBER NAWKR OF_SYM type_denoter'''
-    size = str(int(p[5]) - int(p[3]))
-    print(type1 + id_list.pop(0) + "[" + size + "]", end="")
-
-
+    size= str(int(p[5])-int(p[3]))
+    print(type1 + id_list.pop(0) + "["+size+"]",end="")
 def p_array_type_td(p):
     '''array_type_td : ARRAY_SYM NAWKL NUMBER DOUBLE_DOT NUMBER NAWKR OF_SYM type_denoter_td'''
-    size = str(int(p[5]) - int(p[3]))
-    print(type1 + id_list.pop(0) + "[" + size + "]", end="")
-
-
+    size= str(int(p[5])-int(p[3]))
+    print(type1 + id_list.pop(0) + "["+size+"]",end="")
 def p_record_type(p):
     '''record_type :  record_sym record_section record_section_list  SEMI_COLON rec_end_sym'''
     pass
-
-
 def p_record_type_td(p):
     '''record_type_td :  record_sym_td record_section record_section_list  SEMI_COLON rec_end_sym_td'''
     pass
-
-
 def p_rec_end_sym(p):
     '''rec_end_sym :  END_SYM'''
-    print("}", end="")
-
-
-text = []
-
+    print("}",end="")
 
 def p_rec_end_sym_td(p):
     '''rec_end_sym_td :  END_SYM'''
-    print("}" + text.pop(0), end="")
-
-
+    print("}"+text.pop(0),end="")
 def p_record_sym(p):
     '''record_sym :  RECORD_SYM'''
-    print("struct " + id_list.pop(0) + "{")
-
-
+    print("struct "+id_list.pop(0)+ "{")
 def p_record_sym_td(p):
     '''record_sym_td :  RECORD_SYM'''
-    print("typedef struct " + "{")
+    print("typedef struct "+ "{")
     text.append(id_list.pop(0))
-
-
 def p_record_section_list(p):
     '''record_section_list : record_section_list SEMI_COLON record_section
                     | empty'''
     pass
-
-
 def p_record_section(p):
     '''record_section : id_list COLON type_denoter'''
     print(type1, end=" ")
@@ -409,32 +362,24 @@ def p_record_section(p):
         else:
             print(i, end=";\n")
     id_list.clear()
-
-
 def p_var_block(p):
     '''var_block : VAR_SYM var_decl var_decl_list
                 | empty'''
     pass
-
-
 def p_var_decl(p):
     '''var_decl : id_list COLON type_denoter SEMI_COLON'''
     for i in id_list:
-        if i != id_list[-1]:
-            print(i, end=", ")
+        if i!= id_list[-1]:
+            print(i,end=", ")
         else:
-            print(i, end=";\n")
+            print(i,end=";\n")
     id_list.clear()
-
-
 def p_var_decl_list(p):
     '''var_decl_list : var_decl_list var_decl
                     | empty'''
     pass
 
 
-pf_type = []
-id_list_pom = []
 
 
 def p_procedure_and_function_block(p):
@@ -442,20 +387,17 @@ def p_procedure_and_function_block(p):
                                     | procedure_and_function_block function_decl
                                     | empty'''
     pass
-
-
 def p_procedure_decl(p):
     '''procedure_decl : procedure_header SEMI_COLON block SEMI_COLON'''
-    pass
-
-
+    isMain[0] = True
 def p_procedure_header(p):
     '''procedure_header : PROCEDURE_SYM ID
                         | PROCEDURE_SYM ID NAWL param_section param_section_list NAWR'''
-    print("void " + p[2] + "(", end="")
+    isMain[0] = False
+    print("void " + p[2] + "(",end="")
     for i in id_list_pom:
-        if i != id_list_pom[-1]:
-            print(i, end=", ")
+        if i!= id_list_pom[-1]:
+            print(i,end=", ")
         else:
             print(i, end="")
     print(")", end="\n")
@@ -463,27 +405,22 @@ def p_procedure_header(p):
     pf_type.clear()
     id_list.clear()
 
-
 def p_param_section(p):
     '''param_section : id_list COLON type_general_pf'''
-    id_pom = []
+    id_pom=[]
     for i in id_list:
-        st = pf_type[-1] + " " + i
+        st =pf_type[-1]+" "+i
         id_pom.append(st)
     id_list.clear()
     for i in id_pom:
         id_list.append(i)
     pf_type.clear()
-
-
 def p_param_section_list(p):
     '''param_section_list : param_section_list SEMI_COLON param_section
                         | empty'''
     for i in id_list:
         id_list_pom.append(i)
     id_list.clear()
-
-
 def p_type_general_pf(p):
     '''type_general_pf : CHAR_SYM
                     | INTEGER_SYM
@@ -497,17 +434,14 @@ def p_type_general_pf(p):
         pf_type.append("char")
     elif p[1] == 'boolean':
         pf_type.append("bool")
-
-
 def p_function_decl(p):
     '''function_decl : function_header SEMI_COLON block SEMI_COLON'''
-    pass
-
-
+    isMain[0] = True
 def p_function_header(p):
     '''function_header : FUNCTION_SYM ID COLON type_general_pf
                         | FUNCTION_SYM ID NAWL param_section param_section_list NAWR COLON type_general_pf'''
-    print(pf_type.pop(-1) + " " + p[2] + "(", end="")
+    isMain[0] = False
+    print(pf_type.pop(-1) +" "+ p[2] + "(", end="")
     for i in id_list_pom:
         if i != id_list_pom[-1]:
             print(i, end=", ")
@@ -518,23 +452,28 @@ def p_function_header(p):
     id_list_pom.clear()
     pf_type.clear()
 
-
 def p_error(p):
     print("Syntax error in input!!!")
-
-
 def p_empty(p):
     'empty :'
     pass
 
 
+
+
+
 def p_operation_block(p):
     '''operation_block : BEGIN_SYM statement_sequence END_SYM'''
-
-
+    if isMain[0] is True:
+        print("void main()")
+    print("{")
+    for st in statement_seq:
+        print("\t"+st+";")
+    print("}")
+    statement_seq.clear()
 def p_statement_sequence(p):
     'statement_sequence : statement statement_list'
-    print("seq")
+    pass
 
 
 def p_statement_list(p):
@@ -559,19 +498,19 @@ def p_structured_statement(p):
     '''structured_statement : operation_block
                         | conditional_statement
                         | repetitive_statement'''
-    print("struc")
+    pass
 
 
 def p_repetitive_statement(p):
     '''repetitive_statement : repeat_statement
                         | while_statement
                         | for_statement'''
-    print("rep")
+    pass
 
 
 def p_while_statement(p):
     'while_statement : WHILE_SYM expression DO_SYM statement'
-    print("while ", p[2], ":")
+    pass
 
 
 def p_repeat_statement(p):
@@ -593,7 +532,7 @@ def p_conditional_statement(p):
 def p_if_statement(p):
     """if_statement : IF_SYM THEN_SYM statement else_part
                         | IF_SYM THEN_SYM ELSE_SYM"""
-    print("if")
+    pass
 
 
 def p_else_part(p):
@@ -604,7 +543,7 @@ def p_else_part(p):
 def p_expression(p):
     '''expression : simple_expression relational_operator simple_expression
                         | simple_expression'''
-    print("expression")
+    pass
 
 
 def p_relational_operator(p):
@@ -615,7 +554,13 @@ def p_relational_operator(p):
                         | GREATER
                         | GR_EQ
                         | empty'''
-    print("rel op")
+    if p[1] is not None and len(statement_seq) > 0:
+        if p[1] == "=":
+            statement_seq[-1] = statement_seq[-1] + "=="
+        elif p[1] == "<>":
+            statement_seq[-1] = statement_seq[-1] + "!="
+        else:
+            statement_seq[-1] = statement_seq[-1] + str(p[1])
 
 
 def p_simple_expression(p):
@@ -660,17 +605,13 @@ def p_factor(p):
     '''factor : real_number
             | NAWL expression NAWR
             | NOT_SYM factor'''
-    print("factor")
+    pass
 
 
 def p_assign_statement(p):
-    '''assign_statement : assign_statement_lhs ASSIGN_SYM expression'''
-    print(constdef.pop(-2), "=", constdef.pop())
+    '''assign_statement : id2 ASSIGN_SYM expression'''
+    pass
 
-
-def p_assign_statement_lhs(p):
-    '''assign_statement_lhs : id'''
-    # print(constdef.pop(), end="")
 
 
 def p_procedure_statement(p):
@@ -679,11 +620,25 @@ def p_procedure_statement(p):
                             | WRITELN_SYM NAWL const_value const_value_list NAWR
                             | WRITELN_SYM NAWL NAWR
                             | READLN_SYM NAWL id_list NAWR'''
-    if len(constdef) > 0:
-        print("print(" + constdef.pop() + ")")
-    # else:
-    #     print("print()")
-
+    if p[1]=="writeln" and p[3] is not None:
+        statement_seq.append("printf()")
+    elif p[1]=="writeln" and p[3] is None:
+        stat = "printf(\""
+        for i in constdef:
+            if str(i)[0]=="\"":
+                stat+="s "
+            elif str(i).isnumeric():
+                stat += "d "
+            else:
+                stat += "f "
+        stat+="\","
+        for i in constdef:
+            stat+=str(i)
+            if i!=constdef[-1]:
+                stat+=","
+        stat+=")"
+        constdef.clear()
+        statement_seq.append(stat)
 
 def p_const_value_list(p):
     '''const_value_list : const_value_list COMMA const_value
